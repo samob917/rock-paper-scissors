b5e19fd26f46fb12071f51ba5bafb7b666fa753e
Sam Oberly <samob917@outlook.com>
2024-12-26 22:21:58 -0500

    Add getComputerPick function and test the logic
    Completed the get computer pick component. This randomly picks
    "Rock" "Paper" or "Scissors" based on a random number.

--- rps.js
+++ rps.js
@@ -1,1 +1,11 @@
-console.log("hello, world")
+function getComputerChoice() {
+    let x = Math.random();
+    if (x < 0.33) {
+        return "Rock";
+    } else if (x <= 0.66) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+console.log(getComputerChoice())
